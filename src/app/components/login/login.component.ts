799dc085841c5d3a5c5fc203e1975a5eb20c5090
RobloxGOAT2137 <w.jasinski@zs2wagrowiec.pl>
2025-04-09 19:47:09 +0200

    Update login.component.ts

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { Router, RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatInputModule, MatButtonModule, MatCardModule, FormsModule, MatIconModule, RouterModule],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  onSubmit() {
+    let user = this.userService.login(this.username, this.password);
+    if (!user) {
+      user = this.userService.register(this.username, this.password);
+    }
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem('currentUser', JSON.stringify(user));
+    }
+    this.router.navigate(['/groups']);
+  }
+}
